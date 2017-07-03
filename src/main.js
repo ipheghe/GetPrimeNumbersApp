10a8938b3dfadf5bd8385322f3ffe98ecfab78c9
ovie <iphegheovie@yahoo.com>
2017-07-03 16:11:06 +0100

    update project files

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ module.exports = {
 
             for(var i = 2; i <= n; i++){
 
-                if(i % 2 != 0 && Math.sqrt(i) % 1 !=0 && i % 3 != 0 && i % 5 != 0 ) {
+                if(i % 2 != 0 && Math.sqrt(i) % 1 != 0 && i % 3 != 0 && i % 5 != 0 ) {
 
                  primeArray.push(i); 
                     
